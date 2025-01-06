eb16c21151f9eb5d6727b72cbc762ce7a863304b
henry <13052648+gitpancake@users.noreply.github.com>
2025-01-05 22:34:28 -0800

    Add city specific

--- src/index.ts
+++ src/index.ts
@@ -28,18 +28,50 @@ const main = async () => {
       secret: process.env.WEBHOOK_SECRET,
       flash: { ...flashToPost, img: s3Url },
     });
+    console.log(`Flash posted for ${flashToPost.city}`);
+  } catch (ex) {
+    console.log(ex);
+  }
+};
 
-    console.log("Flash posted");
+const city_specific = async () => {
+  const invaders = await new SpaceInvaders().getLatestFlashes();
+
+  if (!invaders) {
+    console.log("No invaders found");
+    return;
+  }
+
+  const flashes = [...invaders.with_paris, ...invaders.without_paris].filter((x) => x.city.includes("Los Angeles"));
+
+  try {
+    const flashToPost = flashes[Math.round(Math.random() * flashes.length)];
+
+    const s3Url = await uploadImageHandler({
+      imageUrl: `${new SpaceInvaders().IMAGE_BASE}${flashToPost.img}`,
+      key: flashToPost.img,
+    });
+
+    await axios.post(`${process.env.API_URL}/api/bot`, {
+      secret: process.env.WEBHOOK_SECRET,
+      flash: { ...flashToPost, img: s3Url },
+    });
+
+    console.log(`City specific flash posted: ${flashToPost.city}`);
   } catch (ex) {
     console.log(ex);
   }
 };
 
 cron.schedule("* * * * *", () => {
+  city_specific().catch((err) => {
+    console.log(err);
+  });
+});
+
+cron.schedule("*/5 * * * *", () => {
   main()
-    .then(() => {
-      console.log("Done!");
-    })
+    .then(() => {})
     .catch((err) => {
       console.log(err);
     });
