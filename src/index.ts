d9e11295db2a90609160ecaa946be1213f879e64
henry <13052648+gitpancake@users.noreply.github.com>
2025-01-08 16:48:22 -0800

    Silly computer

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,7 @@ import Coordinator from "./util/coordinator";
 
 config({ path: ".env" });
 
-console.log("running");
 cron.schedule("* * * * *", async () => {
-  console.log("running");
   await new Coordinator().fetchFlashes(["Los Angeles", "New York", "Miami", "San Diego"]).catch((err) => {
     console.log(err);
   });
