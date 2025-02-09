31ae2972b7256fe5051e2f5088baca62d195cff0
henry <13052648+gitpancake@users.noreply.github.com>
2025-02-08 22:01:03 -0800

    Alter crontab

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import Coordinator from "./util/coordinator";
 
 config({ path: ".env" });
 
-cron.schedule("* * * * *", async () => {
+cron.schedule("*/5 * * * *", async () => {
   await new Coordinator().fetchFlashes(["Los Angeles", "New York", "Miami", "San Diego"]).catch((err) => {
     console.log(err);
   });
 });
 
-cron.schedule("*/5 * * * *", async () => {
+cron.schedule("*/15 * * * *", async () => {
   await new Coordinator().fetchFlashes().catch((err) => {
     console.log(err);
   });
