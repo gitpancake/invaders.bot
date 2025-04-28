2626d020afcaec8b0d3e000bd56a78f7118cd4ea
henry <13052648+gitpancake@users.noreply.github.com>
2025-04-28 14:11:16 -0700

    Update crons for debug

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import { StoreFlashesCron } from "./util/cron-jobs/store-flashes";
 
 config({ path: ".env" });
 
-new PostRandomFlashCron("*/10 * * * *").register();
+new PostRandomFlashCron("*/1 * * * *").register();
 
-new StoreFlashesCron("*/5 * * * *").register();
+new StoreFlashesCron("*/1 * * * *").register();
 
-new ChannelRefresher("*/59 * * * *").register();
+new ChannelRefresher("*/1 * * * *").register();
