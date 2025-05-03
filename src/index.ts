3b7e8d8b1b41f8552a72d1e9077f2abc1bfafe1c
henry <13052648+gitpancake@users.noreply.github.com>
2025-05-03 14:55:41 -0400

    Put back crons

--- src/index.ts
+++ src/index.ts
@@ -2,34 +2,11 @@ import { config } from "dotenv";
 import { AgenticPoster } from "./util/cron-jobs/agentic-poster";
 import { ChannelRefresher } from "./util/cron-jobs/refresh-bot";
 import { StoreFlashesCron } from "./util/cron-jobs/store-flashes";
-import { FlashesDb } from "./util/mongodb/flashes";
-import { PostPersonalFlash } from "./util/tasks/post-personal";
 
 config({ path: ".env" });
 
-// new AgenticPoster("*/10 * * * *").register();
+new AgenticPoster("*/10 * * * *").register();
 
-// new StoreFlashesCron("*/5 * * * *").register();
+new StoreFlashesCron("*/5 * * * *").register();
 
-// new ChannelRefresher("*/59 * * * *").register();
-
-const testPersonalFlash = async () => {
-  const flash = await new FlashesDb().getRandomDocument({
-    player: "WORLDY",
-  });
-
-  if (!flash) {
-    console.error(`No personal flashes found.`);
-    return;
-  }
-
-  await new PostPersonalFlash().handle(flash);
-};
-
-testPersonalFlash()
-  .then(() => {
-    console.log("Test personal flash completed.");
-  })
-  .catch((err) => {
-    console.error("Error in test personal flash:", err);
-  });
+new ChannelRefresher("*/59 * * * *").register();
