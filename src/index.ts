432f233a96eb09463b36ba510f4c9efb4bd40d99
henry <13052648+gitpancake@users.noreply.github.com>
2025-05-05 08:35:15 -0700

    Disable Flash Post

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { config } from "dotenv";
-import { AgenticPoster } from "./util/cron-jobs/agentic-poster";
 import { ChannelRefresher } from "./util/cron-jobs/refresh-bot";
 import { StoreFlashesCron } from "./util/cron-jobs/store-flashes";
 
 config({ path: ".env" });
 
-new AgenticPoster("*/10 * * * *").register();
+// new AgenticPoster("*/10 * * * *").register();
 
 new StoreFlashesCron("*/5 * * * *").register();
 
