b81cad728b23c58fd42608f8e1159a580dc8595a
henry <13052648+gitpancake@users.noreply.github.com>
2025-05-08 17:36:43 -0700

    Reconnect

--- src/util/mongodb/connector.ts
+++ src/util/mongodb/connector.ts
@@ -1,30 +1,47 @@
-import { Collection, Document } from "mongodb";
-import clientPromise from "./mongoClient";
+import { Collection, Db, Document, MongoClient } from "mongodb";
+
+let mongoClient: MongoClient | null = null;
 
 export abstract class Mongo<T extends Document> {
-  public collection: Collection<T>;
-  private dbName: string;
-  private collectionName: string;
+  protected db!: Db;
+  protected collection!: Collection<T>;
 
-  constructor({ dbName, collectionName }: { dbName: string; collectionName: string }) {
-    this.dbName = dbName;
-    this.collectionName = collectionName;
-    // collection will be set in execute
-    this.collection = undefined as any;
+  constructor(private options: { dbName: string; collectionName: string }) {}
+
+  private async getClient(): Promise<MongoClient> {
+    const uri = process.env.DATABASE_URL!;
+    if (!mongoClient) {
+      mongoClient = new MongoClient(uri, {
+        maxPoolSize: 10,
+        serverSelectionTimeoutMS: 5000,
+      });
+      await mongoClient.connect();
+    } else {
+      try {
+        // Ensure connection is still alive
+        await mongoClient.db("admin").command({ ping: 1 });
+      } catch (err) {
+        console.warn("Reconnecting to Mongo...");
+        mongoClient = new MongoClient(uri, {
+          maxPoolSize: 10,
+          serverSelectionTimeoutMS: 5000,
+        });
+        await mongoClient.connect();
+      }
+    }
+    return mongoClient;
   }
 
-  public async onConnect(): Promise<void> {}
+  public async connect(): Promise<void> {
+    const client = await this.getClient();
+    this.db = client.db(this.options.dbName);
+    this.collection = this.db.collection<T>(this.options.collectionName);
+  }
 
-  protected async execute<R>(operation: (collection: Collection<T>) => Promise<R>): Promise<R> {
-    const client = await clientPromise;
-    this.collection = client.db(this.dbName).collection<T>(this.collectionName);
-
-    try {
-      await this.onConnect();
-      return await operation(this.collection);
-    } catch (error) {
-      console.error("Database operation error:", error);
-      throw error;
+  protected async execute<R>(fn: (col: Collection<T>) => Promise<R>): Promise<R> {
+    if (!this.collection) {
+      await this.connect();
     }
+    return fn(this.collection);
   }
 }
