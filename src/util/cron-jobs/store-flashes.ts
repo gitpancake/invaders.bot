01a54121357705f519cfbd1c9d31181106607faa
henry <13052648+gitpancake@users.noreply.github.com>
2025-06-29 16:54:26 -0700

    Add RabbitMQ publish

--- src/util/cron-jobs/store-flashes.ts
+++ src/util/cron-jobs/store-flashes.ts
@@ -1,6 +1,6 @@
 import { PostgresFlashesDb } from "../database/invader-flashes";
 import SpaceInvadersAPI from "../flash-invaders";
-import { InvaderFlashCache } from "../image-sync";
+import { RabbitImagePush } from "../rabbitmq";
 import { formattedCurrentTime } from "../times";
 import { CronTask } from "./base";
 
@@ -24,19 +24,19 @@ export class StoreFlashesCron extends CronTask {
     try {
       const writtenDocuments = await new PostgresFlashesDb().writeMany(flattened);
 
-      const uploadCount = await new InvaderFlashCache().batchUpload(
-        flattened
-          .filter((flash) => {
-            return writtenDocuments.some((doc) => Number(doc.flash_id) === flash.flash_id);
-          })
-          .map((flash) => ({
-            imageUrl: `${invaderApi.API_URL}${flash.img}`,
-            key: flash.img,
-          }))
-      );
+      const flashesToPublish = flattened.filter((flash) => {
+        return writtenDocuments.some((doc) => Number(doc.flash_id) === flash.flash_id);
+      });
 
-      if (uploadCount > 0 || writtenDocuments.length > 0) {
-        console.log(`${flattened.length} flashes. ${uploadCount} new images. ${writtenDocuments.length} new documents. ${formattedCurrentTime()}`);
+      const rabbit = new RabbitImagePush();
+      let publishCount = 0;
+      for (const flash of flashesToPublish) {
+        await rabbit.publish(flash);
+        publishCount++;
+      }
+
+      if (publishCount > 0 || writtenDocuments.length > 0) {
+        console.log(`${flattened.length} flashes. ${publishCount} new events published. ${writtenDocuments.length} new documents. ${formattedCurrentTime()}`);
       }
     } catch (error) {
       console.error("Error storing flashes:", error);
