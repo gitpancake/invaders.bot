64404121a25a31b7991053f4ca35f3adda7b02f1
henry <13052648+gitpancake@users.noreply.github.com>
2025-07-13 11:04:15 -0700

    Change frequency of storeFlashes cron

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { StoreFlashesCron } from "./util/cron-jobs/store-flashes";
 
 config({ path: ".env" });
 
-const storeFlashesCron = new StoreFlashesCron("*/5 * * * *");
+const storeFlashesCron = new StoreFlashesCron("*/7 * * * *");
 storeFlashesCron.task();
 storeFlashesCron.register();
 
